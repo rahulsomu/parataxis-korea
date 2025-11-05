9bbdb2c84be27d344eb72ceacd734b569ded1843
rahul rana <rana.rahul.1998@gmail.com>
2025-11-05 10:30:06 +0530

    added tinyeditor

--- src/components/htmlEditor/HtmlEditor.jsx
+++ src/components/htmlEditor/HtmlEditor.jsx
@@ -1,37 +1,86 @@
-import CKEditor from "ckeditor4-react";
-import React from "react";
+// import CKEditor from "ckeditor4-react";
+import React, { useEffect, useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 const HtmlEditor = ({ value, onChange, id }) => {
-  const config = {
-    allowedContent: true,
-    versionCheck: false,
-    // Remove modal-based plugins
-    removePlugins:
-      "dialog,dialogadvtab,image,link,table,flash,forms,iframe,about",
-    // Keep toolbar minimal + include Source toggle
-    toolbar: [
-      { name: "document", items: ["Source"] },
-      {
-        name: "basicstyles",
-        items: ["Bold", "Italic", "Underline", "Strike", "-", "RemoveFormat"],
-      },
-      {
-        name: "paragraph",
-        items: ["NumberedList", "BulletedList", "-", "Outdent", "Indent"],
-      },
-      { name: "clipboard", items: ["Undo", "Redo"] },
-      { name: "editing", items: ["Find", "Replace"] },
-    ],
+  const [content, setContent] = useState(value || "");
+  const apikey = process.env.REACT_APP_TINY_API_KEY;
+  // const config = {
+  //   allowedContent: true,
+  //   versionCheck: false,
+  //   // Remove modal-based plugins
+  //   removePlugins:
+  //     "dialog,dialogadvtab,image,link,table,flash,forms,iframe,about",
+  //   // Keep toolbar minimal + include Source toggle
+  //   toolbar: [
+  //     { name: "document", items: ["Source"] },
+  //     {
+  //       name: "basicstyles",
+  //       items: ["Bold", "Italic", "Underline", "Strike", "-", "RemoveFormat"],
+  //     },
+  //     {
+  //       name: "paragraph",
+  //       items: ["NumberedList", "BulletedList", "-", "Outdent", "Indent"],
+  //     },
+  //     { name: "clipboard", items: ["Undo", "Redo"] },
+  //     { name: "editing", items: ["Find", "Replace"] },
+  //   ],
+  // };
+  const handleEditorChange = (newContent, editor) => {
+    setContent(newContent);
   };
+  useEffect(() => {
+    if (content) {
+      onChange(content, id, true);
+    }
+  }, [content]);
+
   return (
-    <CKEditor
-      type="classic"
-      data={value}
-      onChange={(event) => {
-        onChange(event.editor.getData(), id, true);
-      }}
-      config={config}
-    />
+    // <CKEditor
+    //   type="classic"
+    //   data={value}
+    //   onChange={(event) => {
+    //     onChange(event.editor.getData(), id, true);
+    //   }}
+    //   config={config}
+    // />/
+    <>
+      <Editor
+        apiKey={apikey}
+        // initialValue={content}
+        value={content}
+        init={{
+          height: 500,
+          menubar: true,
+          plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "code",
+            "help",
+            "wordcount",
+          ],
+          toolbar:
+            "undo redo | blocks | " +
+            "bold italic forecolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+        }}
+        onEditorChange={(newcontent) => handleEditorChange(newcontent)}
+      />
+    </>
   );
 };
 
